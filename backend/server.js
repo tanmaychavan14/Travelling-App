498920a6102c617622276368c9bf6f035b878c73
tanmaychavan14 <139693308+tanmaychavan14@users.noreply.github.com>
2025-01-01 11:57:48 +0530

    ee

--- backend/server.js
+++ backend/server.js
@@ -12,17 +12,16 @@ require('dotenv').config();
 mongodb();
 app.use(express.json());
 app.use(cookieParser());
-const corsOptions = {
-    origin: 'https://travelifyyy.netlify.app', // Replace with your frontend domain
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  };
-  
-  app.use(cors(corsOptions));
-  
-
-  app.options('*', cors(corsOptions));
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://travelifyyy.netlify.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true'); // For cookies
+    if (req.method === 'OPTIONS') {
+        return res.status(204).end();
+    }
+    next();
+});
 
 
 
